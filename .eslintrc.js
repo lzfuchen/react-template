aa0eb145647b0b5ae4629384e8f0a7527cb03fe7
fuchen <284992984@qq.com>
2022-12-22 09:31:59 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+const OFF = 0
+const WARN = 1
+const ERROR = 2
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['import', 'react', 'jsx-a11y', 'unicorn', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'unicorn/better-regex': ERROR,
+    'unicorn/prevent-abbreviations': OFF,
+    'unicorn/filename-case': [
+      ERROR,
+      {
+        cases: {
+          // 中划线
+          kebabCase: false,
+          // 小驼峰
+          camelCase: true,
+          // 下划线
+          snakeCase: false,
+          // 大驼峰
+          pascalCase: true
+        }
+      }
+    ],
+    'unicorn/no-array-instanceof': WARN,
+    'unicorn/no-for-loop': WARN,
+    'unicorn/prefer-add-event-listener': [
+      ERROR,
+      {
+        excludedPackages: ['koa', 'sax']
+      }
+    ],
+    'unicorn/prefer-query-selector': ERROR,
+    'unicorn/no-null': OFF,
+    'unicorn/no-array-reduce': OFF,
+    'unicorn/no-array-for-each': OFF,
+    'unicorn/consistent-destructuring': OFF,
+    'unicorn/prefer-module': OFF,
+    'unicorn/consistent-function-scoping': OFF,
+    'unicorn/no-abusive-eslint-disable': OFF,
+    'unicorn/numeric-separators-style': OFF,
+    'unicorn/prefer-object-from-entries': OFF,
+    'unicorn/explicit-length-check': OFF,
+    'unicorn/prefer-export-from': OFF,
+    'unicorn/prefer-code-point': OFF,
+    'unicorn/no-useless-fallback-in-spread': OFF,
+    'unicorn/prefer-spread': OFF
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  //缩进级别的空格数
+  tabWidth: 2,
+  // 换行的行长
+  printWidth: 120,
+  // 没有尾随逗号
+  trailingComma: 'none',
+  // 箭头函数括号
+  arrowParens: 'always',
+  // 每个语句的末尾去除分号
+  semi: false,
+  // 使用单引号而不是双引号
+  singleQuote: true,
+  endOfLine: 'auto',
+  proseWrap: 'never'
+}
